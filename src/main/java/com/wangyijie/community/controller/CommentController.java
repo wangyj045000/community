c1820cb2853d3eb78950cab14ed5fd1d436311d0
wangyj045000 <wyj045000@gmail.com>
2020-06-18 18:50:24 +0800

    kafka first

--- src/main/java/com/wangyijie/community/controller/CommentController.java
+++ src/main/java/com/wangyijie/community/controller/CommentController.java
@@ -1,8 +1,12 @@
 package com.wangyijie.community.controller;
 
-
 import com.wangyijie.community.entity.Comment;
+import com.wangyijie.community.entity.DiscussPost;
+import com.wangyijie.community.entity.Event;
+import com.wangyijie.community.event.EventProducer;
 import com.wangyijie.community.service.CommentService;
+import com.wangyijie.community.service.DiscussPostService;
+import com.wangyijie.community.util.CommunityConstant;
 import com.wangyijie.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,7 +18,7 @@ import java.util.Date;
 
 @Controller
 @RequestMapping("/comment")
-public class CommentController {
+public class CommentController implements CommunityConstant {
 
     @Autowired
     private CommentService commentService;
@@ -22,13 +26,36 @@ public class CommentController {
     @Autowired
     private HostHolder hostHolder;
 
-    @RequestMapping(path = "/add/{discussPostId}",method = RequestMethod.POST)
-    public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment){
+    @Autowired
+    private EventProducer eventProducer;
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
+    public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
         comment.setUserId(hostHolder.getUser().getId());
         comment.setStatus(0);
         comment.setCreateTime(new Date());
         commentService.addComment(comment);
 
+        // 触发评论事件
+        Event event = new Event()
+                .setTopic(TOPIC_COMMENT)
+                .setUserId(hostHolder.getUser().getId())
+                .setEntityType(comment.getEntityType())
+                .setEntityId(comment.getEntityId())
+                .setData("postId", discussPostId);
+        if (comment.getEntityType() == ENTITY_TYPE_POST) {
+            DiscussPost target = discussPostService.findDiscussPostById(comment.getEntityId());
+            event.setEntityUserId(target.getUserId());
+        } else if (comment.getEntityType() == ENTITY_TYPE_COMMENT) {
+            Comment target = commentService.findCommentById(comment.getEntityId());
+            event.setEntityUserId(target.getUserId());
+        }
+        eventProducer.fireEvent(event);
+
         return "redirect:/discuss/detail/" + discussPostId;
     }
+
 }
--- src/main/java/com/wangyijie/community/controller/FollowController.java
+++ src/main/java/com/wangyijie/community/controller/FollowController.java
@@ -1,7 +1,9 @@
 package com.wangyijie.community.controller;
 
+import com.wangyijie.community.entity.Event;
 import com.wangyijie.community.entity.Page;
 import com.wangyijie.community.entity.User;
+import com.wangyijie.community.event.EventProducer;
 import com.wangyijie.community.service.FollowService;
 import com.wangyijie.community.service.UserService;
 import com.wangyijie.community.util.CommunityConstant;
@@ -30,12 +32,24 @@ public class FollowController implements CommunityConstant {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private EventProducer eventProducer;
+
     @RequestMapping(path = "/follow", method = RequestMethod.POST)
     @ResponseBody
     public String follow(int entityType, int entityId) {
         User user = hostHolder.getUser();
 
         followService.follow(user.getId(), entityType, entityId);
+
+        // 触发关注事件
+        Event event = new Event()
+                .setTopic(TOPIC_FOLLOW)
+                .setUserId(hostHolder.getUser().getId())
+                .setEntityType(entityType)
+                .setEntityId(entityId)
+                .setEntityUserId(entityId);
+        eventProducer.fireEvent(event);
 
         return CommunityUtil.getJSONString(0, "已关注!");
     }
@@ -107,4 +121,3 @@ public class FollowController implements CommunityConstant {
     }
 
 }
-
